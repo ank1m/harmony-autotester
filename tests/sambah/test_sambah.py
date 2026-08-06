443da2e3ab966f671e4338488cdeb06a99637bb7
Kim Cunningham <kimberly.cunningham@nasa.gov>
2026-08-06 07:53:38 -0400

    added prints

--- tests/sambah/test_sambah.py
+++ tests/sambah/test_sambah.py
@@ -37,9 +37,11 @@ def test_sambah(failed_tests, harmony_client, service_collection, earthaccess_lo
         granule_names = [get_granule_filename(granule) for granule in granules]
         batch_indices = get_batch_indices(granule_names)
 
-        grouped = _group_batch_indices(batch_indices, granule_names)
 
+        grouped = _group_batch_indices(batch_indices, granule_names)
+        print(f'grouped {grouped}')
         scans = sorted(grouped.values(), key=len)
+        print(f'scans {scans}')
         assert scans, 'No compatible scans were found'
 
         if len(scans) > 1:
